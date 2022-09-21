6a83d743f832dde07f1cf801d1501c52522b1f5b
Igli-Boci <igliboci10@gmai.com>
2022-09-21 02:30:29 +0200

    first commit

---
+++ server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import cors from "cors";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const port = 3010;
+
+const prisma = new PrismaClient();
